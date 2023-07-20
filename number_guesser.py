6300697a5673ad6e351a97f5fb7bbca827751bdb
Erik Sanchez <erikcsanchezdev@gmail.com>
2023-07-19 21:07:43 -0500

    added to readme

--- number_guesser.py
+++ number_guesser.py
@@ -1,5 +1,16 @@
 import random
 
-r = random.randrange(-5, 11)
 
-print(r)
+top_of_range = input("Type a number: ")
+
+if top_of_range.isdigit():
+    top_or_range = int(top_of_range)
+
+    if top_of_range <= 0: 
+        print("Please type a number larger than 0 next time.")
+        quit()
+else:
+    print("Please type a number next time.")
+    quit()
+random_number = random.randint(11)
+
