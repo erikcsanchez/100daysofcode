5f41a344e35586daf40a561472c3aaf614d1e193
Erik Sanchez <erikcsanchezdev@gmail.com>
2023-08-02 19:57:53 -0500

    continued expense tracker in react...

--- expense-tracker-react/src/components/TransactionList.js
+++ expense-tracker-react/src/components/TransactionList.js
@@ -1,13 +1,17 @@
-import React from 'react'
-
+import React, {useContext} from 'react'
+import {GlobalContext} from '../context/GlobalState'
+import { Transactions } from './Transactions';
 export const TransactionList = () => {
+  const {transactions} = useContext(GlobalContext);
+
+
+
   return (
     <>
         <h3>History</h3>
-      <ul classN="list">
-        <li className="minus">
-          Cash <span>-$400</span><button className="delete-btn">x</button>
-        </li> 
+      <ul className="list">
+        {transactions.map(transaction => (<Transactions key={transaction.id} transaction={transaction}/> ))}
+        
       </ul>
     </>
   )
